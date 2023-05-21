0bb35cbfe634bc3256f653564d3a5781f0a16f68
dogeThinkpad <dogessbm@gmail.com>
2023-05-21 16:12:45 -0400

    Automatic commit.

---
+++ MapData.h
@@ -0,0 +1,34 @@
+#ifndef MAPDATA_H
+#define MAPDATA_H
+
+#define MAPDATA { \
+    {_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BE,_BE,_BE,_BW,_BE,_BW,_BE,_BE,_BE,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW},\
+    {_BW,_BS,_BS,_BL,_BS,_BS,_BS,_BS,_BS,_BW,_BE,_BE,_BE,_BW,_BE,_BW,_BE,_BE,_BE,_BW,_BS,_BS,_BS,_BS,_BW,_BW,_BS,_BS,_BS,_BS,_BW},\
+    {_BW,_BS,_BW,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BE,_BE,_BE,_BW,_BE,_BW,_BE,_BE,_BE,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BE,_BW,_BS,_BW,_BW,_BS,_BW,_BE,_BE,_BE,_BW,_BE,_BW,_BE,_BE,_BE,_BW,_BS,_BW,_BW,_BS,_BS,_BS,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BE,_BW,_BS,_BW,_BW,_BS,_BW,_BE,_BE,_BE,_BW,_BE,_BW,_BE,_BE,_BE,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BE,_BW,_BW,_BW,_BW,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BW,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BE,_BW,_BW,_BW,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BE,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BE,_BW,_BW,_BW,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BE,_BW,_BS,_BS,_BS,_BS,_BW,_BW,_BE,_BE,_BE,_BE,_BE,_BE,_BE,_BS,_BS,_BS,_BW,_BW,_BS,_BS,_BS,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BE,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BE,_BW,_BW,_BW,_BW,_BW,_BE,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BE,_BW,_BE,_BE,_BE,_BW,_BE,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BS,_BS,_BS,_BS,_BW,_BW,_BS,_BS,_BS,_BE,_BW,_BE,_BE,_BE,_BW,_BE,_BW,_BW,_BS,_BS,_BS,_BS,_BW,_BW,_BS,_BS,_BS,_BS,_BW},\
+    {_BW,_BW,_BW,_BW,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BE,_BE,_BE,_BE,_BE,_BW,_BE,_BW,_BW,_BW,_BW,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BS,_BW},\
+    {_BW,_BW,_BW,_BW,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BE,_BE,_BE,_BE,_BE,_BW,_BE,_BW,_BW,_BW,_BW,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BS,_BS,_BS,_BS,_BW,_BW,_BS,_BS,_BS,_BE,_BW,_BE,_BE,_BE,_BW,_BE,_BW,_BW,_BS,_BS,_BS,_BS,_BW,_BW,_BS,_BS,_BS,_BS,_BW},\
+    {_BW,_BS,_BW,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BE,_BW,_BE,_BE,_BE,_BW,_BE,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BE,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BE,_BW,_BW,_BW,_BW,_BW,_BE,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BE,_BW,_BS,_BS,_BS,_BS,_BW,_BW,_BE,_BE,_BE,_BE,_BE,_BE,_BE,_BS,_BS,_BS,_BW,_BW,_BS,_BS,_BS,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BE,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BE,_BW,_BW,_BW,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BW,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BE,_BW,_BW,_BW,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BE,_BW,_BW,_BW,_BW,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BE,_BW,_BS,_BW,_BW,_BS,_BW,_BE,_BE,_BE,_BW,_BE,_BW,_BE,_BE,_BE,_BW,_BS,_BW,_BW,_BW,_BW,_BW,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BE,_BW,_BS,_BW,_BW,_BS,_BW,_BE,_BE,_BE,_BW,_BE,_BW,_BE,_BE,_BE,_BW,_BS,_BW,_BW,_BS,_BS,_BS,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BW,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BE,_BE,_BE,_BW,_BE,_BW,_BE,_BE,_BE,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BS,_BW,_BW,_BS,_BW},\
+    {_BW,_BS,_BS,_BL,_BS,_BS,_BS,_BS,_BS,_BW,_BE,_BE,_BE,_BW,_BE,_BW,_BE,_BE,_BE,_BW,_BS,_BS,_BS,_BL,_BW,_BW,_BS,_BS,_BS,_BS,_BW},\
+    {_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BE,_BE,_BE,_BW,_BE,_BW,_BE,_BE,_BE,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW,_BW}}\
+
+#endif /* end of include guard: MAPDATA_H */
--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include "DogeLib/Includes.h"
-#include "tiles.h"
-#include "spawn.h"
+#include "Tiles.h"
+#include "MapData.h"
+#include "Spawn.h"
 typedef enum{PS_WAIT, PS_NORM, PS_BLUE, PS_DEAD, PS_WIN, PS_NUM}PlayerState;
 const char *PlayerStateStr[PS_NUM] = {"PS_WAIT", "PS_NORM", "PS_BLUE", "PS_DEAD", "PS_WIN"};
 
-typedef struct
-{
+typedef struct{
     Direction dir;
     Direction turn;
     PlayerState state;
     Coord pos;
-} Player, Ghost;
+}Player, Ghost;
 
 void playerDraw(const Player player)
 {
@@ -23,8 +23,7 @@ Player playerGetInput(Player player)
     const Scancode wasd[4] = {SDL_SCANCODE_W, SDL_SCANCODE_D, SDL_SCANCODE_S, SDL_SCANCODE_A};
     bool keydown = false;
     for(uint i = 0; i < 4; i++){
-        if (keyPressed(wasd[i]) && !keyPressed(wasd[(i + 2) % 4]))
-        {
+        if(keyPressed(wasd[i]) && !keyPressed(wasd[(i + 2) % 4])){
             keydown = true;
             player.turn = i;
         }
@@ -33,10 +32,8 @@ Player playerGetInput(Player player)
         player.turn = player.dir;
         return player;
     }
-    for (uint i = 0; i < 4; i++)
-    {
-        if (keyState(wasd[i]) && !keyState(wasd[(i+2)%4]))
-        {
+    for(uint i = 0; i < 4; i++){
+        if(keyState(wasd[i]) && !keyState(wasd[(i+2)%4])){
             player.turn = i;
         }
     }
@@ -45,18 +42,16 @@ Player playerGetInput(Player player)
 
 Player playerMove(Player player, int map[28][31])
 {
-    if (player.pos.x % TILE_LEN == TILE_HLF && player.pos.x % TILE_LEN == TILE_HLF){
+    if(player.pos.x % TILE_LEN == TILE_HLF && player.pos.x % TILE_LEN == TILE_HLF){
         const Coord pos = coordDivi(coordAddi(player.pos, -TILE_HLF), TILE_LEN);
         const Coord dirPos = coordShift(pos, player.dir, 1);
         const Coord turnPos = coordShift(pos, player.turn, 1);
-        if (!inMap(turnPos) || map[turnPos.x][turnPos.y] == TILE_ID_WALL)
-        {
-            if (inMap(dirPos) && map[dirPos.x][dirPos.y] != TILE_ID_WALL){
+        if(!inMap(turnPos) || map[turnPos.x][turnPos.y] == TILE_ID_WALL){
+            if(inMap(dirPos) && map[dirPos.x][dirPos.y] != TILE_ID_WALL){
                 player.pos = coordShift(player.pos, player.dir, 1);
                 return player;
             }
-        }
-        else if (inMap(turnPos) && map[turnPos.x][turnPos.y] != TILE_ID_WALL){
+        }else if(inMap(turnPos) && map[turnPos.x][turnPos.y] != TILE_ID_WALL){
                 player.dir = player.turn;
                 player.pos = coordShift(player.pos, player.dir, 1);
                 return player;
@@ -78,35 +73,7 @@ int main(void)
     setWindowSize(TILE_X*TILE_LEN, TILE_Y*TILE_LEN);
 
     // load the board
-    int map[TILE_X][TILE_Y] = {
-        {_BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BE, _BE, _BE, _BW, _BW, _BE, _BE, _BE, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW},
-        {_BW, _BS, _BS, _BL, _BS, _BS, _BS, _BS, _BS, _BW, _BE, _BE, _BE, _BW, _BW, _BE, _BE, _BE, _BW, _BS, _BS, _BS, _BS, _BS, _BL, _BS, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BE, _BE, _BE, _BW, _BW, _BE, _BE, _BE, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BE, _BE, _BE, _BW, _BW, _BE, _BE, _BE, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BE, _BE, _BE, _BW, _BW, _BE, _BE, _BE, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BS, _BS, _BS, _BW, _BW, _BS, _BS, _BS, _BS, _BS, _BS, _BW, _BW, _BS, _BS, _BS, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BE, _BE, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BS, _BS, _BS, _BS, _BW, _BW, _BS, _BS, _BS, _BS, _BW, _BE, _BE, _BW, _BS, _BS, _BS, _BS, _BW, _BW, _BS, _BS, _BS, _BS, _BS, _BW},
-        {_BW, _BW, _BW, _BW, _BW, _BS, _BW, _BW, _BW, _BW, _BW, _BS, _BE, _BE, _BE, _BE, _BS, _BW, _BW, _BW, _BW, _BW, _BS, _BW, _BW, _BW, _BW, _BW},
-        {_BW, _BW, _BW, _BW, _BW, _BS, _BW, _BW, _BW, _BW, _BW, _BS, _BE, _BE, _BE, _BE, _BS, _BW, _BW, _BW, _BW, _BW, _BS, _BW, _BW, _BW, _BW, _BW},
-        {_BW, _BS, _BS, _BS, _BS, _BS, _BW, _BW, _BS, _BS, _BS, _BS, _BW, _BE, _BE, _BW, _BS, _BS, _BS, _BS, _BW, _BW, _BS, _BS, _BS, _BS, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BE, _BE, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BS, _BS, _BS, _BW, _BW, _BS, _BS, _BS, _BS, _BS, _BS, _BW, _BW, _BS, _BS, _BS, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BE, _BE, _BE, _BW, _BW, _BE, _BE, _BE, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BE, _BE, _BE, _BW, _BW, _BE, _BE, _BE, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BW, _BW, _BW, _BS, _BW, _BW, _BS, _BW, _BE, _BE, _BE, _BW, _BW, _BE, _BE, _BE, _BW, _BS, _BW, _BW, _BS, _BW, _BW, _BW, _BS, _BW},
-        {_BW, _BS, _BS, _BL, _BS, _BS, _BS, _BS, _BS, _BW, _BE, _BE, _BE, _BW, _BW, _BE, _BE, _BE, _BW, _BS, _BS, _BS, _BS, _BS, _BL, _BS, _BS, _BW},
-        {_BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BE, _BE, _BE, _BW, _BW, _BE, _BE, _BE, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW, _BW}};
+    int map[TILE_X][TILE_Y] = MAPDATA;
 
     Player player = {
         .dir = DIR_L,
